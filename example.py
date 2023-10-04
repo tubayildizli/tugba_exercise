152ab84b963c462255c6b45a13be2afc89a3c38a
Joao Vargas <joao.francisco.vargas@gmail.com>
2023-10-04 15:57:49 +0200

    Fixed testing issue

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ def test_add():
 	assert add('space', 'ship') == 'spaceship'
 
 def subtract(a, b):
-    return a + b  # do not change this line until prompted to do so
+    return a - b  # do not change this line until prompted to do so
 
 def test_subtract():
 	assert  subtract(2,3) == -1
